8b053b21bf5ae7ca0d14bb06e997d07ae987b7c5
joewutaiwan <jantijoe.cs00g@g2.nctu.edu.tw>
2017-02-12 12:03:15 +0000

    [APP]
    - add app flow

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-var Retriver = require('./twse_request/twse_request.js');
+var TwseRequest = require('./twse_request/twse_request.js');
+var HtmlParser = require('./html_parser/html_parser.js');
+var util = require("util");
 
 var para = {
-	type: 1,
-	company : 2495,
-	year: 105,
-	season: 3
-}
+	type: 4,
+	company : '2498',
+	year: '104',
+	season: '04'
+};
 
-var callback = function (resp) { 
-	if (!resp.success) {
-		console.log(resp.body);
+var HtmlParserCallback = function (resp) {
+	if (resp.success) {
+		console.log(resp.data);
+	} else {
+		console.log('[fail] HtmlParserCallback testcase type');
+		console.log('error :' + resp.error);
 	}
 }
 
-Retriver.getData(para, callback);
+var TwseRequestCallback = function (resp) { 
+	if (resp.success) {
+		HtmlParser.parse(para, resp.body, HtmlParserCallback);
+	} else {
+		console.log('[fail] TwseRequest testcase type :' + resp.para.type);
+		console.log('error :' + resp.error);
+		console.log('httpResponse :' + resp.httpResponse);
+		console.log('body :' + resp.body);
+	}
+}
 
+TwseRequest.getData(para, TwseRequestCallback);
